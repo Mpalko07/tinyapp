30ba20f42d6cc05a30a15e875f474cba90ef1540
Monica Palko <m.a.palko07@gmail.com>
2024-03-04 21:30:07 -0500

    implemented logout

--- express_server.js
+++ express_server.js
@@ -22,13 +22,26 @@ function generateRandomString() {
 }
 
 app.post("/urls", (req, res) => {
-  const longURL = req.body.longURL; // Retrieve the longURL from the request body
-  const shortURL = generateRandomString(); // Generate a shortURL
-  urlDatabase[shortURL] = longURL; // Write the shortURL and longURL to the urlDatabase
-  res.redirect(`/urls/${shortURL}`); // Redirect to /urls/:id
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.set("view engine", "ejs");
+
+// Login route
+app.post("/login", (req, res) => {
+  const { username } = req.body;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+// Logout route
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -45,26 +58,27 @@ app.get("/hello", (req, res) => {
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
+});
 
- app.get("/urls/new", (req, res) => {
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+});
+
+app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
- app.get("/urls", (req, res) => {
+app.get("/urls", (req, res) => {
   const templateVars = {
     username: req.cookies["username"],
-    urls: urlDatabase };
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
-  const longURL = urlDatabase[id]; // Retrieve the long URL from the database based on the short URL ID
+  const longURL = urlDatabase[id];
   if (longURL) {
     const templateVars = { id: id, longURL: longURL };
     res.render("urls_show", templateVars);
@@ -86,18 +100,11 @@ app.get("/u/:id", (req, res) => {
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
   if (urlDatabase.hasOwnProperty(id)) {
-    delete urlDatabase[id]; // Use delete operator to remove the URL from the database
-    res.redirect("/urls"); // Redirect back to the urls_index page ("/urls")
+    delete urlDatabase[id];
+    res.redirect("/urls");
   } else {
     res.status(404).send("Short URL not found");
   }
-});
-
-// POST route for handling login
-app.post("/login", (req, res) => {
-  const username = req.body.username; 
-  res.cookie("username", username); 
-  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {

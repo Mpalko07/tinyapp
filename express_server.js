54d3ad0b1a7f90c7bfd2216c24db357608cccedc
Monica Palko <m.a.palko07@gmail.com>
2024-03-04 20:26:30 -0500

    Modify _header.ejs to display a username using cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -54,7 +56,9 @@ app.get("/set", (req, res) => {
 });
 
  app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 

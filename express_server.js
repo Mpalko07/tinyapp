f8e234306d9d70e86c28311cbfd69fd8c1cf085d
Monica Palko <m.a.palko07@gmail.com>
2024-03-04 19:39:17 -0500

    added post route for /login and redirect browser back to /urls page

--- express_server.js
+++ express_server.js
@@ -89,6 +89,13 @@ app.post("/urls/:id/delete", (req, res) => {
   }
 });
 
+// POST route for handling login
+app.post("/login", (req, res) => {
+  const username = req.body.username; 
+  res.cookie("username", username); 
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });

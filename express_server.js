d4b5da4faca5b70bb5f827f0f282d672888ce66d
Monica Palko <m.a.palko07@gmail.com>
2024-03-14 20:28:50 -0400

    Modified getUserByEmail function to take in the user's email and users database as parameters

--- express_server.js
+++ express_server.js
@@ -32,8 +32,13 @@ function generateRandomString() {
 }
 
 // Function to get user by email
-const getUserByEmail = (email) => {
-  return Object.values(users).find(user => user.email === email);
+const getUserByEmail = function(email, database) {
+  for (const userId in database) {
+    if (database[userId].email === email) {
+      return database[userId];
+    }
+  }
+  return null;
 };
 
 // Function to associate URLs with specific users
